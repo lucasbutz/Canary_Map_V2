956536f6824fad81248211fd2f52858bff5eabd2
Lucas BuchlI <lucasb26@uw.edu>
2026-04-27 15:32:46 -0700

    failed pop up + read me update

--- Map/src/main.js
+++ Map/src/main.js
@@ -2,9 +2,27 @@ import './style.css'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
-const map = new maplibregl.Map({
-  container: 'map',
-  style: 'https://tiles.openfreemap.org/styles/liberty',
-  center: [-122.3321, 47.6062],
-  zoom: 12
-})
+const Seattle = [-122.3321, 47.6062]
+
+// async function holderName() {
+//   try {}
+
+
+// } 
+
+async function loadMaplibre() {
+  const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    center: Seattle,
+    zoom: 12
+  })
+
+  new maplibregl.Popup({
+    closeOnClick = true
+  })
+    .setLngLat(Seattle)
+    .setHTML("<h3>Test Pin</h3>")
+    .addTo(map);
+    
+}
