499ab0708f8142052c2b2598cc13d78593c792be
Lucas BuchlI <lucasb26@uw.edu>
2026-04-27 16:38:55 -0700

    added test pop-up, worked on local host, waiting on vite

--- Map/src/main.js
+++ Map/src/main.js
@@ -4,13 +4,6 @@ import 'maplibre-gl/dist/maplibre-gl.css'
 
 const Seattle = [-122.3321, 47.6062]
 
-// async function holderName() {
-//   try {}
-
-
-// } 
-
-async function loadMaplibre() {
   const map = new maplibregl.Map({
     container: 'map',
     style: 'https://tiles.openfreemap.org/styles/liberty',
@@ -18,11 +11,14 @@ async function loadMaplibre() {
     zoom: 12
   })
 
-  new maplibregl.Popup({
-    closeOnClick = true
+  map.on('load', () => {
+    new maplibregl.Popup({ closeOnClick: true })
+      .setLngLat(Seattle)
+      .setHTML("<h3>Test Pin</h3>")
+      .addTo(map)
   })
-    .setLngLat(Seattle)
-    .setHTML("<h3>Test Pin</h3>")
-    .addTo(map);
-    
-}
+
+
+
+
+
